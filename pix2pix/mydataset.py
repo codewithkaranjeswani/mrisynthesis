0311a56576ead9ca92efd78da4c824a89b977672
codewithkaranjeswani <krnjeswani21@gmail.com>
2020-06-17 04:09:26 +0530

    Feature with sample images

--- pix2pix/mydataset.py
+++ pix2pix/mydataset.py
@@ -2,27 +2,16 @@ import random
 import os
 import numpy as np
 import nibabel as nib
-import glob
 
+import torch
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
-import torchvision.transforms as transforms
 
 class MRI_T1_CT_Dataset(Dataset):
 	def __init__(self, root):
-		self.transform = transforms.ToTensor()
 		data_dir = os.listdir(root)
 		ct_fn = sorted(os.listdir(root + '/' + data_dir[0]))
 		mr_fn = sorted(os.listdir(root + '/' + data_dir[1]))
-		# print(ct_fn)
-		# print(mr_fn)
-
-		# ct_fn = sorted(glob.glob(root + '/' + data_dir[0]))
-		# mr_fn = sorted(glob.glob(root + '/' + data_dir[1]))
-		# print(ct_fn)
-		# print(mr_fn)
-
-		# exit(2)
 		self.datalist = []
 		for i, (mr_file, ct_file) in enumerate(zip(mr_fn, ct_fn)):
 			assert mr_file[:3] == ct_file[:3], "MRI and CT files not from the same patient"
@@ -39,27 +28,30 @@ class MRI_T1_CT_Dataset(Dataset):
 				mr_img_data = np.array(mr_img_data)
 				assert mr_img_data.shape == ct_img_data.shape, "MRI and CT have different shapes"
 				for j in range(20):
-					self.datalist.append([ np.uint8(mr_img_data[:,:,j]), np.uint8(ct_img_data[:,:,j]) ])
-				# print(mr_file, mr_img_data.shape, ct_file, ct_img_data.shape)
+					mr_in = mr_img_data[:,:,j]
+					mr_in = torch.from_numpy(mr_in)
+					ct_in = ct_img_data[:,:,j]
+					ct_in = torch.from_numpy(ct_in)
+
+					mr_max = torch.max(mr_in)
+					ct_max = torch.max(ct_in)
+					mr_min = torch.min(mr_in)
+					ct_min = torch.min(ct_in)
+
+					mr_final = torch.div( (mr_in - mr_min), (1.0 *(mr_max - mr_min)) )
+					ct_final = torch.div( (ct_in - ct_min), (1.0 *(ct_max - ct_min)) )
+
+					mr_final = mr_final.reshape(1, mr_final.shape[0], mr_final.shape[1])
+					ct_final = ct_final.reshape(1, ct_final.shape[0], ct_final.shape[1])
+
+					mr_final = mr_final.type(torch.FloatTensor)
+					ct_final = ct_final.type(torch.FloatTensor)
+
+					self.datalist.append([ mr_final, ct_final ])
 
 	def __getitem__(self, index):
 		[mr_img, ct_img] = self.datalist[index]
-		img_A = self.transform(mr_img)
-		img_B = self.transform(ct_img)
-		return {"A": img_A, "B": img_B}
+		return {"A": mr_img, "B": ct_img}
 
 	def __len__(self):
 		return len(self.datalist)
-
-# data_root_dir = 'RIRE-ct-t1'
-# mydata = MRI_T1_CT_Dataset(data_root_dir)
-# # print(mydata[24]['A'].shape, mydata[24]['B'].shape)
-# print(mydata[24])
-
-# dataloader = DataLoader(
-# 	MRI_T1_CT_Dataset("../../Processed_Data/%s" % data_root_dir),
-# 	batch_size=opt.batch_size,
-# 	shuffle=True,
-# 	num_workers=opt.n_cpu,
-# )
-
